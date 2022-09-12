9fb4412fec705433db1424aacb1b635914b79df1
Joe Hawkins <hawkinsjwh@gmail.com>
2022-09-12 12:26:02 -0600

    added assembly

--- prac.py
+++
@@ -1,7 +0,0 @@
-counter = 0
-
-
-while counter != 10:
-
-    counter += 1
-    print(counter)
---
+++ pratice.asm
@@ -0,0 +1,7 @@
+; Assembly Language
+
+
+;this is a comment in Assembly
+;this is another comment in Assembly
+
+
